92363e475b93eecee607e2b9f73a9085ca9ced2f
LuisAngelNV <deanda.1998@gmail.com>
2025-05-25 22:01:05 -0600

    first commit

---
+++ src/main/java/com/miempresa/App.java
@@ -0,0 +1,10 @@
+package com.miempresa;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SaludoService servicio = new SaludoService();
+        servicio.saludar("Luis");
+    }
+}
---
+++ src/main/java/com/miempresa/SaludoService.java
@@ -0,0 +1,7 @@
+package com.miempresa;
+
+public class SaludoService {
+    public void saludar(String nombre) {
+        System.out.println("Hola, " + nombre + "! Este proyecto ya es Mavenizado.");
+    }
+}
